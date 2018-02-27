25f3684d3337ae626833efc538dce54470119f82
amankh <amankh@cmu.edu>
2018-02-27 18:22:42 -0500

    update + new part

--- assignment1/04_pascal_vgg.py
+++ assignment1/04_pascal_vgg.py
@@ -21,14 +21,13 @@ tf.logging.set_verbosity(tf.logging.INFO)
 plot_map = False
 log_map = True
 
+
 debug_data_loader = 0 # set to 1 to debug using prints
 test_cnn = 0# set to 1 to train/test only 20 images
-test_cnn_batch = 100
+test_cnn_batch = 50
 BATCH_SIZE = 10
-NUM_ITERS = 400
-LOG_DIR = 'logs/task2_2'
-
-
+NUM_ITERS = 50
+LOG_DIR = 'logs/task4_1'
 
 CLASS_NAMES = [
     'aeroplane',
@@ -52,6 +51,40 @@ CLASS_NAMES = [
     'train',
     'tvmonitor',
 ]
+vgg_mapping = { 'vgg_16/conv1/conv1_1/biases': 'conv2d/bias',
+            'vgg_16/conv1/conv1_1/weights': 'conv2d/kernel',
+            'vgg_16/conv1/conv1_2/biases': 'conv2d_1/bias',
+            'vgg_16/conv1/conv1_2/weights': 'conv2d_1/kernel',
+            'vgg_16/conv2/conv2_1/biases': 'conv2d_2/bias',
+            'vgg_16/conv2/conv2_1/weights': 'conv2d_2/kernel',
+            'vgg_16/conv2/conv2_2/biases': 'conv2d_3/bias',
+            'vgg_16/conv2/conv2_2/weights': 'conv2d_3/kernel',
+            'vgg_16/conv3/conv3_1/biases': 'conv2d_4/bias',
+            'vgg_16/conv3/conv3_1/weights': 'conv2d_4/kernel',
+            'vgg_16/conv3/conv3_2/biases': 'conv2d_5/bias',
+            'vgg_16/conv3/conv3_2/weights': 'conv2d_5/kernel',
+            'vgg_16/conv3/conv3_3/biases': 'conv2d_6/bias',
+            'vgg_16/conv3/conv3_3/weights': 'conv2d_6/kernel',
+            'vgg_16/conv4/conv4_1/biases': 'conv2d_7/bias',
+            'vgg_16/conv4/conv4_1/weights': 'conv2d_7/kernel',
+            'vgg_16/conv4/conv4_2/biases': 'conv2d_8/bias',
+            'vgg_16/conv4/conv4_2/weights': 'conv2d_8/kernel',
+            'vgg_16/conv4/conv4_3/biases': 'conv2d_9/bias',
+            'vgg_16/conv4/conv4_3/weights': 'conv2d_9/kernel',
+            'vgg_16/conv5/conv5_1/biases': 'conv2d_10/bias',
+            'vgg_16/conv5/conv5_1/weights': 'conv2d_10/kernel',
+            'vgg_16/conv5/conv5_2/biases': 'conv2d_11/bias',
+            'vgg_16/conv5/conv5_2/weights': 'conv2d_11/kernel',
+            'vgg_16/conv5/conv5_3/biases': 'conv2d_12/bias',
+            'vgg_16/conv5/conv5_3/weights': 'conv2d_12/kernel',
+            'vgg_16/fc6/biases': 'conv2d_13/bias',
+            'vgg_16/fc6/weights': 'conv2d_13/kernel',
+            'vgg_16/fc7/biases': 'conv2d_14/bias',
+            'vgg_16/fc7/weights': 'conv2d_14/kernel'}
+
+class vgg_weights(tf.train.SessionRunHook):
+     def begin(self):
+         tf.contrib.framework.init_from_checkpoint('vgg_16.ckpt', vgg_mapping)
 
 def summary_var(log_dir, name, val, step):
     writer = tf.summary.FileWriterCache.get(log_dir)
@@ -62,33 +95,10 @@ def summary_var(log_dir, name, val, step):
     writer.add_summary(summary_proto, step)
     writer.flush()
 
-
 def cnn_model_fn(features, labels, mode, num_classes=20):
     '''
-    ALEXNET ARCHITECTURE:
+    VGG config D ARCHITECTURE:
     -> image        [-1, 256, 256, 3]
-    -> data augmentation [-1,224,224,3]
-
-    -> conv(11, 4, 96, 'VALID')  [-1, 256-11/4 +1 , ]
-    -> relu()
-    -> max_pool(3, 2)
-    -> conv(5, 1, 256, 'SAME')
-    -> relu()
-    -> max_pool(3, 2)
-    -> conv(3, 1, 384, 'SAME')
-    -> relu()
-    -> conv(3, 1, 384, 'SAME')
-    -> relu()
-    -> conv(3, 1, 256, 'SAME')
-    -> max_pool(3, 2)
-    -> flatten()
-    -> fully_connected(4096)
-    -> relu()
-    -> dropout(0.5)
-    -> fully_connected(4096)
-    -> relu()
-    -> dropout(0.5)
-    -> fully_connected(20)
     '''
 
     input_layer2 = tf.reshape(features["x"], [-1,256,256,3])
@@ -96,133 +106,212 @@ def cnn_model_fn(features, labels, mode, num_classes=20):
   
     #data augmentation
     if mode == tf.estimator.ModeKeys.TRAIN:
-        input_layer = tf.random_crop(input_layer2,
+        input_layer2 = tf.random_crop(input_layer2,
             size = [BATCH_SIZE,224,224,3])
-        input_layer = tf.map_fn(lambda img: tf.image.random_flip_left_right(img), input_layer)
+        input_layer = tf.map_fn(lambda img: tf.image.random_flip_left_right(img), input_layer2)
         
     if mode == tf.estimator.ModeKeys.PREDICT:
         input_layer = tf.map_fn(lambda img: tf.image.resize_image_with_crop_or_pad(img, 224, 224), input_layer2)
-        #input_layer = tf.image.crop_to_bounding_box(input_layer2,
-        #    offset_height = 16, offset_width= 16,
-        #    target_height = 224, target_width = 224)
 
     #conv layer1
     conv1 = tf.layers.conv2d(
         inputs= input_layer,
-        filters = 96,
-        strides = 4,
-        kernel_size = [11,11],
-        padding= "valid",
+        filters = 64,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
         activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
-
-    #pool1
-    pool1 = tf.layers.max_pooling2d(
-        inputs=conv1,
-        pool_size =[3,3],
-        strides=2)
+        use_bias = True)
 
     #conv layer2
     conv2 = tf.layers.conv2d(
-        inputs= pool1,
-        filters = 256,
+        inputs= conv1,
+        filters = 64,
         strides = 1,
-        kernel_size = [5,5],
+        kernel_size = [3,3],
         padding= "same",
         activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
+        use_bias = True)
 
-    #pool2
-    pool2 = tf.layers.max_pooling2d(
+    #pool1
+    pool1 = tf.layers.max_pooling2d(
         inputs=conv2,
-        pool_size =[3,3],
+        pool_size =[2,2],
         strides=2)
 
     #conv layer3
     conv3 = tf.layers.conv2d(
-        inputs= pool2,
-        filters = 384,
+        inputs= pool1,
+        filters = 128,
         strides = 1,
         kernel_size = [3,3],
         padding= "same",
         activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
+        use_bias = True)
 
     #conv layer4
     conv4 = tf.layers.conv2d(
-        inputs= conv3,
-        filters = 384,
+        inputs=conv3 ,
+        filters = 128,
         strides = 1,
         kernel_size = [3,3],
         padding= "same",
         activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
+        use_bias = True)
     
+    #pool2
+    pool2 = tf.layers.max_pooling2d(
+        inputs=conv4,
+        pool_size =[2,2],
+        strides=2)
+
     #conv layer5
     conv5 = tf.layers.conv2d(
-        inputs= conv4,
+        inputs= pool2,
         filters = 256,
         strides = 1,
         kernel_size = [3,3],
         padding= "same",
-        activation = tf.nn.relu,  ## check if activation is there or not
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
+        activation = tf.nn.relu,
+        use_bias = True)
+    
+    #conv layer6
+    conv6 = tf.layers.conv2d(
+        inputs= conv5,
+        filters = 256,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
+
+    #conv layer7
+    conv7 = tf.layers.conv2d(
+        inputs= conv6,
+        filters = 256,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
 
     #pool3
     pool3 = tf.layers.max_pooling2d(
-        inputs=conv5,
-        pool_size =[3,3],
+        inputs=conv7,
+        pool_size =[2,2],
         strides=2)
 
-    #flatten
-    pool3_flat = tf.reshape(pool3, [-1,5*5*256])
-
-    # fc layer 1
-    dense1 = tf.layers.dense(
-        inputs = pool3_flat,
-        units = 4096,
+    #conv layer8
+    conv8 = tf.layers.conv2d(
+        inputs= pool3,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
         activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
+        use_bias = True)
+    
+    #conv layer9
+    conv9 = tf.layers.conv2d(
+        inputs= conv8,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
 
+    #conv layer10
+    conv10 = tf.layers.conv2d(
+        inputs= conv9,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
+    
+    #pool4
+    pool4 = tf.layers.max_pooling2d(
+        inputs=conv10,
+        pool_size =[2,2],
+        strides=2)
+
+    #conv layer11
+    conv11 = tf.layers.conv2d(
+        inputs= pool4,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
+    
+    #conv layer12
+    conv12 = tf.layers.conv2d(
+        inputs= conv11,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
+
+    #conv layer13
+    conv13 = tf.layers.conv2d(
+        inputs= conv12,
+        filters = 512,
+        strides = 1,
+        kernel_size = [3,3],
+        padding= "same",
+        activation = tf.nn.relu,
+        use_bias = True)
+    
+    #pool5
+    pool5 = tf.layers.max_pooling2d(
+        inputs=conv13,
+        pool_size =[2,2],
+        strides=2)
+
+    # fc layer 6
+    dense1 = tf.layers.conv2d(
+        inputs= pool5,
+        filters = 4096,
+        strides = 1,
+        kernel_size = [7,7],
+        padding= "valid",
+        activation = tf.nn.relu,
+        use_bias = True)
+    
+    
     #drropout1
     dropout1 = tf.layers.dropout(
         inputs = dense1,
         rate = 0.5,
         training = mode== tf.estimator.ModeKeys.TRAIN)
-
-    #fc layer 2
-    dense2 = tf.layers.dense(
-        inputs = dropout1,
-        units = 4096,
-        activation = tf.nn.relu,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
     
-    #dropout2
-    dropout2 = tf.layers.dropout(
+    # fc layer 7
+    dense2 = tf.layers.conv2d(
+        inputs=  dropout1,
+        filters = 4096,
+        strides = 1,
+        kernel_size = [1,1],
+        padding= "valid",
+        activation = tf.nn.relu,
+        use_bias = True)
+
+    #drropout2
+    dropout2= tf.layers.dropout(
         inputs = dense2,
         rate = 0.5,
         training = mode== tf.estimator.ModeKeys.TRAIN)
 
-    #fc layer 2
-    dense3 = tf.layers.dense(inputs = dropout2,units = 20,
-        use_bias = True,
-        bias_initializer = tf.zeros_initializer(),
-        kernel_initializer = tf.random_normal_initializer(0, 0.01))
-
+    dense2_flat = tf.reshape(dropout2,[-1,4096])
+    
+    #fc layer 3
+    dense3 = tf.layers.dense(
+        inputs= dense2_flat,
+        units = 20)
 
     ## check this part
     predictions = {
@@ -236,13 +325,12 @@ def cnn_model_fn(features, labels, mode, num_classes=20):
     # Calculate Loss (for both TRAIN and EVAL modes)
     loss = tf.identity(tf.losses.sigmoid_cross_entropy(multi_class_labels=labels, logits=dense3), name='loss')
 
-
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-
         #learning schedule
         starter_learning_rate = 0.001
         decay_steps = 10000
+
         learning_rate = tf.train.exponential_decay(
             learning_rate = starter_learning_rate,
             global_step = tf.train.get_global_step(),
@@ -251,15 +339,20 @@ def cnn_model_fn(features, labels, mode, num_classes=20):
         tf.summary.scalar(name ="learning_rate", tensor=learning_rate)
         #tf.summary.image(name = "image", tensor =input_layer, max_outputs = BATCH_SIZE)
         
-        optimizer = tf.train.MomentumOptimizer(learning_rate= learning_rate, momentum = 0.9)
+
+        optimizer = tf.train.MomentumOptimizer(learning_rate= learning_rate, momentum = 0.9)      
+        grad_list = optimizer.compute_gradients(loss)
+        for index, grad in grad_list:
+            if index is not None:
+                tf.summary.histogram("{}-grad".format(grad.name), index)
+
+        #for index, grad in enumerate(grad_list):
+        #    tf.summary.histogram("{}-grad".format(grad_list[index][1].name), grad_list[index])
+
         train_op = optimizer.minimize(
             loss=loss,
             global_step=tf.train.get_global_step())
-        return tf.estimator.EstimatorSpec(
-            mode=mode, loss=loss, train_op=train_op)
-
-    # Add evaluation metrics (for EVAL mode)
-    #eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions["classes"])}
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss)
 
@@ -297,7 +390,7 @@ def load_pascal(data_dir, split='train'):
     data_list = data_list_file.readlines()
     data_list = [d.strip() for d in data_list if d.strip()]  #removing whitespaces and terminal characters
     data_list_file.close()
-
+  
     #initializing data/labels/weight/ arrays
     number_of_images = len(data_list) #number of data points
     print(split ,'data size :', number_of_images, '\n')
@@ -308,6 +401,7 @@ def load_pascal(data_dir, split='train'):
     labels = np.empty((number_of_images, 20), dtype = "int")
     weights = np.empty((number_of_images, 20), dtype = "int")
 
+    
     #loading images
     for i in range(number_of_images):
         
@@ -413,7 +507,7 @@ def main():
     tensors_to_log = {"loss": "loss", 
     "global_step" : "global_step"}
     logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=100)
+        tensors=tensors_to_log, every_n_iter=1)
     
     # Train the model
     train_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -434,15 +528,18 @@ def main():
         plt.figure(1)
         plt.ion()
     if log_map == True:
-        #gtmapFile = open('task2_map/gt_map.txt','w')
-        #ranmapFile = open('task2_map/ran_map.txt','w')
-        #evalmapFile = open('task2_map/eval_map.txt','w')
-        meanmapFile = open('task2_mean_map.txt','w')
+        #gtmapFile = open('task4_map/gt_map.txt','w')
+        #ranmapFile = open('task4_map/ran_map.txt','w')
+        #evalmapFile = open('task4_map/eval_map.txt','w')
+        meanmapFile = open('task4_mean_map.txt','w')
+
+    #initializing with VGG-16 weights
+    vgg_init_weights = vgg_weights()
+    pascal_classifier.train(input_fn=train_input_fn, steps=NUM_ITERS, hooks=[logging_hook, vgg_init_weights])
 
     #print(pascal_classifier.get_variable_names())
 
-
-    for it in range(100):
+    for it in range(80):
         print("SET :", it)
         
         #train
@@ -450,6 +547,7 @@ def main():
             input_fn=train_input_fn,
             steps=NUM_ITERS,
             hooks=[logging_hook])
+
         
         #compute mAP
         pred = list(pascal_classifier.predict(input_fn=eval_input_fn))
@@ -469,10 +567,7 @@ def main():
         for cid, cname in enumerate(CLASS_NAMES):
             print('{}: {}'.format(cname, _get_el(AP, cid)))
             class_map = _get_el(AP,cid)
-            summary_var(LOG_DIR, '{} mAP'.format(cname) ,class_map, it*NUM_ITERS)
-            
-            
-
+            summary_var(LOG_DIR, '{} mAP'.format(cname) , class_map, it*NUM_ITERS)
         
         #writing map to tensorboard
         summary_var(LOG_DIR, "Obtained_mAP", np.mean(AP), it*NUM_ITERS)
@@ -488,6 +583,9 @@ def main():
     
     if plot_map == True: plt.ioff()
     if log_map == True: meanmapFile.close()
+
+
+
 
 
 if __name__ == "__main__":
